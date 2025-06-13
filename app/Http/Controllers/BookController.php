8ca851e871d23c701c483b606e8ef94026e1fe72
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-06-13 21:50:39 +0700

    feat(feature/index): added function index connect with api to testing in the thunder client

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Return a JSON response when user calls /api/books
+        return response()->json([
+            // Message to confirm the API call was successful
+            'message' => 'Book successfully',
+
+            // 'data' contains one book object with full structure
+            'data' => [
+                'id' => '1a2b3c4d',
+                'title' => 'Learn Laravel',
+                'authorId' => 'author123',
+                'isbn' => '978-1-23456-789-0',
+                'publicationYear' => 2024,
+                'genre' => 'Programming',
+                'availableCopies' => 3
+            ]
+        ]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController; // Correct import with capital A in App
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| Here you register your API routes for Laravel.
+| These routes are loaded by the RouteServiceProvider inside the "api" middleware group.
 |
 */
 
+// Route to call index() in BookController
+Route::get('/books', [BookController::class, 'index']);
+
+// Example route with auth middleware (optional)
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
