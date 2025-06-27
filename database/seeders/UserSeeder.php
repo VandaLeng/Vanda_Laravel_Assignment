098f7f10cd6c97bc3329aaee4c47a057d057f1e2
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-06-27 10:04:58 +0700

    feat(feature/Factory): added file Userfactory to count of fake data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,19 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash; // Needed for password hashing
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        User::create([
-            'name' => 'Vandaq Leng',
-            'email' => 'vandaleng22@gmail.com',
-            'password' => Hash::make('password1234'), // Always hash passwords
-        ]);
+        User::updateOrCreate(
+            ['email' => 'vandaaleng22@gmail.com'], // search by email
+            [
+                'name' => 'VandaaLeng',
+                'password' => Hash::make('password1234'),
+            ]
+        );
     }
 }
